2ce8ab8b56dadab4c2cc137eb3300a85b19634fe
fjgsxbn <chrdwh@outlook.com>
2025-07-26 09:57:13 +0800

    Update a.js

--- a.js
+++ a.js
@@ -3,6 +3,6 @@ async function run(){
 
   let r= await fetch('https://api.cmc.hebtv.com/scms/api/com/article/getArticleList?catalogId=32557&siteId=1')
   let j = await r.json()
-  console.log(j)
+  console.log(j)uuu
 
 }
