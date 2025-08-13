ed35070c4ae9742b26ee3c787267f874fc9f9208
fjgsxbn <chrdwh@outlook.com>
2025-08-13 19:28:17 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,9 +1,9 @@
 async function(){
-  return [{
+  return JSON.stringify([{
     name:'ds',url:'https://tv.pull.hebtv.com/jishi/weishipindao.m3u8?t=1755089833&k=eceb21d175171671b224e75913204d7a'
   },{
     name:'gg',url:'https://tv.pull.hebtv.com/jishi/weishipindao.m3u8?t=1755089833&k=eceb21d175171671b224e75913204d7a'
-  }]
+  }])
 }
 
 
