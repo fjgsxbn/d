b3522f11aeb4aac0e535f1b39910b7acb6288ece
fjgsxbn <chrdwh@outlook.com>
2025-08-13 19:32:29 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-async function(){
+async function hb(){
   return JSON.stringify([{
     name:'ds',url:'https://tv.pull.hebtv.com/jishi/weishipindao.m3u8?t=1755089833&k=eceb21d175171671b224e75913204d7a'
   },{
