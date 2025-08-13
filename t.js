0d12121f496dacc8183145c7a3cd7cad2f059794
fjgsxbn <chrdwh@outlook.com>
2025-08-13 19:27:28 +0800

    Update t.js

--- t.js
+++ t.js
@@ -1,5 +1,5 @@
 async function(){
-  return[{
+  return [{
     name:'ds',url:'https://tv.pull.hebtv.com/jishi/weishipindao.m3u8?t=1755089833&k=eceb21d175171671b224e75913204d7a'
   },{
     name:'gg',url:'https://tv.pull.hebtv.com/jishi/weishipindao.m3u8?t=1755089833&k=eceb21d175171671b224e75913204d7a'
