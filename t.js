2898037a2aacd042af15c0841c1a12522c46c487
fjgsxbn <chrdwh@outlook.com>
2025-08-15 14:36:31 +0800

    Update t.js

--- t.js
+++ t.js
@@ -5,7 +5,9 @@ async function hb(){
     name:'gg',url:'https://0472.org/hls/cgtne.m3u8'
   }])
 }
-
+setInterval(async ()=>
+  hb().then(ar=>xtv.update(ar))
+            ,5000)
 hb()
 //hb().then(ar=>xtv.update(ar))
 
