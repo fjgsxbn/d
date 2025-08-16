c0121ec5d46b08bf3df7cea867b66a2bff0c5f4e
fjgsxbn <chrdwh@outlook.com>
2025-08-16 23:51:27 +0800

    Update t.js

--- t.js
+++ t.js
@@ -5,9 +5,10 @@ async function hb(){
     name:'gg',url:'https://0472.org/hls/cgtne.m3u8'
   }])
 }
+hb().then(ar=>xtv.update(ar))
 setInterval(async ()=>{hb().then(ar=>xtv.update(ar))},5000)
 
-hb().then(ar=>xtv.update(ar))
+
 function h(){
   return JSON.stringify([{
     name:'ds',url:'https://0472.org/hls/cgtn.m3u8'
